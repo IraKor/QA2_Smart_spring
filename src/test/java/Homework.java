070926438fb0019772579817cc9a44e4db2463c4
user <rr@rr.lv>
2019-02-04 22:11:13 +0200

    homework for check

---
+++ src/test/java/Homework.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+public class Homework {
+    @Test
+    public void HomeworkResult() {
+            int Loan = 100000;
+            int Interest1 = 10;
+            int Interest2 = 8;
+            int Interest3 = 6;
+            int Years = 10;
+
+            int TotalInterest1 = Loan * Interest1 * Years;
+            int TotalInterest2 = Loan * Interest2 * Years;
+            int TotalInterest3 = Loan * Interest3 * Years;
+
+
+
+
+    }
+
+    }
+
