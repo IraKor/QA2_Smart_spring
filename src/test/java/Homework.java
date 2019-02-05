761519f117323c8c200e11cd128215cf92f27573
user <rr@rr.lv>
2019-02-05 15:33:18 +0200

    homework for review

--- src/test/java/Homework.java
+++ src/test/java/Homework.java
@@ -3,17 +3,30 @@ import org.junit.jupiter.api.Test;
 public class Homework {
     @Test
     public void HomeworkResult() {
-            int Loan = 100000;
-            int Interest1 = 10;
-            int Interest2 = 8;
-            int Interest3 = 6;
-            int Years = 10;
+            int loan = 80500;
+            int interest1 = 10;
+            int interest2 = 8;
+            int interest3 = 6;
+            int years = 10;
+            int rate = 100;
+            int totalYears = 30;
+            int principal = loan / totalYears;
+            int principal10Years = principal * years;
+            int totalInterest1 = loan * interest1 * years / rate;
 
-            int TotalInterest1 = Loan * Interest1 * Years;
-            int TotalInterest2 = Loan * Interest2 * Years;
-            int TotalInterest3 = Loan * Interest3 * Years;
+            int loanNext10Years = loan - principal10Years;
+            int totalInterest2 = loanNext10Years * interest2 * years / rate;
 
-            System.out.println( "Interest for first 10 years: " + TotalInterest1);
+            int loanLast10Years = loanNext10Years - principal10Years;
+            int totalInterest3 = loanLast10Years * interest3 * years / rate;
+
+            int grandTotalInterests = totalInterest1 + totalInterest1 + totalInterest3;
+            int grandTotalAmountPaid = loan + grandTotalInterests;
+
+            System.out.println("Interest for first 10 years: " + totalInterest1);
+            System.out.println("Interests for next 10 years: " + totalInterest2);
+            System.out.println("Interests for last 10 years: " + totalInterest3);
+            System.out.println("Grand total amount paid: " +grandTotalAmountPaid);
 
 
 
