08a874b59da0972a521afe85812e37cea55d1762
user <rr@rr.lv>
2019-04-03 14:17:46 +0300

    Initial commit

--- src/test/java/DelfiFirstTest.java
+++ src/test/java/DelfiFirstTest.java
@@ -1,9 +1,15 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DelfiFirstTest {
 
+
+
+    private final By TITLE = By.xpath(".//h1[contains(@class, 'headline__title')]");
 
     @Test
     public void delfiFirstTest() {
@@ -11,6 +17,9 @@ public class DelfiFirstTest {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("http://www.delfi.lv");
+
+        WebElement firstTitle = driver.findElement(TITLE);
+        String titleText = firstTitle.getText();
 
     }
 }
